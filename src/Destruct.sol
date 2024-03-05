446962c7191d15023b051c5711a05f1c5f47aa6b
SchneiderNejc <nejc.sch@gmail.com>
2024-03-05 23:13:15 +0100

    add Destruct.sol

---
+++ src/Destruct.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+/// @notice use older version since selfdestruct was deprecated in 0.8.18
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+contract Destruct {
+}
