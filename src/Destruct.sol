4004fc13c27b3308f5c43046d991600909e0ba85
SchneiderNejc <nejc.sch@gmail.com>
2024-03-05 23:13:29 +0100

    add title

--- src/Destruct.sol
+++ src/Destruct.sol
@@ -3,5 +3,16 @@
 pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ *  @title Destruct refunding mechanism
+ *  @author Nejc Schneider
+ *  @notice In addition to refunding ether, this contract also refunds 
+ *          custom ERC20 tokens upon selfdestruct.
+ *  @dev There is currently no way to specify which erc20 tokens are
+ *       accepted, so contract will receive all.
+ *  @dev There is also no conventional way to keep track of different erc20 tokens sent
+ *       to contract, so only the one specified in the constructor will be refunded.
+ */
 contract Destruct {
 }
